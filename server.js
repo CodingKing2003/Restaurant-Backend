48d1c6007103726958f5ac4cc5ee0013ab54a6e9
Rajan <rajanj448@student.sfit.ac.in>
2024-03-03 18:24:43 +0530

    MVC Pattern Nodejs

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const morgan=require("morgan");
 require("dotenv").config();
 
 
+
+
+
 app.use(cors());
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+// routes
+
+app.use("/api/v1/test",require("./routes/testRouter"));
 
 
 const PORT=process.env.PORT ||8080 ;
